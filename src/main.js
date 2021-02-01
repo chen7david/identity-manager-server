af17048c523ed2f57f8f21429ee0f29f49ca24fa
David Chen <chen7david@me.com>
2021-02-01 10:46:45 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 const Koa = require('koa')
 const app = new Koa()
+const cors = require('kcors')
+const bodyparser = require('koa-bodyparser')
 const router = require('./routes')
 const { server } = require('config')
 const { cargo, errors, mutator, logger } = require('cargo-io')
 
 /* APP MIDDLEWARE */
+app.use(cors())
 app.use(cargo())
+app.use(bodyparser())
 app.use(errors(mutator()))
 app.on('error', logger)
 
